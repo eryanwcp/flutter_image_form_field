ac0efe6cafbba73dcc75447c396e29d0e4541ace
eryan <eryanwcp@gmail.com>
2021-10-08 09:25:18 +0800

    自定义

--- lib/src/types.dart
+++ lib/src/types.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 import 'package:flutter/widgets.dart' show Widget, BuildContext;
+import 'package:image_picker/image_picker.dart';
 
 typedef BuildImagePreviewCallback<T> = Widget Function(BuildContext, dynamic);
 typedef BuildButton = Widget Function(BuildContext, int);
-typedef InitializeFileAsImageCallback<T> = T Function(File);
+typedef InitializeFileAsImageCallback<T> = T Function(PickedFile);
