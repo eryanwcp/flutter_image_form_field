c65c5434f92a54dcebf5ceac3791a004cc1adbf8
eryan <eryanwcp@gmail.com>
2021-10-18 15:23:02 +0800

    自定义

--- lib/src/preview.dart
+++ lib/src/preview.dart
@@ -28,11 +28,11 @@ class _ImagePreview<T> extends StatelessWidget {
             onTap: onRemove,
             child: const Icon(
               Icons.close,
-              color: const Color(0xFFFFFFFF)
-            )
+              color: Color(0xFFFFFFFF),
+            ),
           )
-        ]
-      )
+        ],
+      ),
     );
   }
 }
@@ -40,7 +40,7 @@ class _ImagePreview<T> extends StatelessWidget {
 class ImagesPreview<T> extends StatefulWidget {
   const ImagesPreview({
     required this.controller,
-    required this.previewImageBuilder
+    required this.previewImageBuilder,
   });
 
   final ImageFieldController<T> controller;
@@ -57,9 +57,10 @@ class _ImagesPreviewState<T> extends State<ImagesPreview> {
     if (image == null) return Container();
 
     return _ImagePreview<T>(
-        image: image,
-        previewImageBuilder: widget.previewImageBuilder,
-        onRemove: () => widget.controller.remove(image));
+      image: image,
+      previewImageBuilder: widget.previewImageBuilder,
+      onRemove: () => widget.controller.remove(image),
+    );
   }
 
   @override
@@ -67,18 +68,22 @@ class _ImagesPreviewState<T> extends State<ImagesPreview> {
     if (images?.isEmpty ?? true) return Container();
 
     return Container(
-        margin: const EdgeInsets.only(top: 10.0),
-        child: images!.length == 1
-            ? SizedBox(child: buildImage(images!.first))
-            : SizedBox(
-                height: 150.0,
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      mainAxisSpacing: 10.0, crossAxisCount: 1),
-                  itemCount: images!.length,
-                  itemBuilder: (_, idx) => buildImage(images![idx]),
-                  scrollDirection: Axis.horizontal,
-                )));
+      margin: const EdgeInsets.only(top: 10.0),
+      child: images!.length == 1
+          ? SizedBox(child: buildImage(images!.first))
+          : SizedBox(
+              height: 150.0,
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  mainAxisSpacing: 10.0,
+                  crossAxisCount: 1,
+                ),
+                itemCount: images!.length,
+                itemBuilder: (_, idx) => buildImage(images![idx]),
+                scrollDirection: Axis.horizontal,
+              ),
+            ),
+    );
   }
 
   void _setImages() {
